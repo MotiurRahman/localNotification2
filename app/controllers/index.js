07a36dd7edfdb55a0a685c4bec3c94c1af55dced
motiurrahman <motiur.mbstu@gmail.com>
2015-11-24 17:21:47 +0600

    second

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,9 +5,9 @@ function doClick(e) {
 var intent = Ti.Android.createServiceIntent({
 	url : 'ExampleService.js'
 });
-intent.putExtra('title', 'Richard III');
+
 intent.putExtra('message', 'Now is the winter of our discontent...');
-intent.putExtra('timestamp', new Date(new Date().getTime() + 30 * 1000));
+//intent.putExtra('timestamp', new Date(new Date().getTime() + 30 * 1000));
 intent.putExtra('interval', 3000);
 Ti.Android.startService(intent);
 
--- app/lib/ExampleService.js
+++ app/lib/ExampleService.js
@@ -1,30 +1,35 @@
 var service = Ti.Android.currentService;
 var serviceIntent = service.getIntent();
-var timestamp = new Date(serviceIntent.getStringExtra('timestamp'));
-if (new Date() > timestamp) {
-    // Grab extra data sent with the intent
-    var title = serviceIntent.getStringExtra('title');
-    var message = serviceIntent.getStringExtra('message');
-    
-    // Create an intent that launches the application
-    var intent = Ti.Android.createIntent({
-        action : Ti.Android.ACTION_MAIN,
-        className: 'com.bd.localNotificationService.localNotificationServiceActivity',
-        packageName: 'com.bd.localNotificationService'
-    });
-    intent.flags |= Ti.Android.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED | Ti.Android.FLAG_ACTIVITY_SINGLE_TOP;
-    intent.addCategory(Ti.Android.CATEGORY_LAUNCHER);
-  
-    // Create notification
-    var notification = Ti.Android.createNotification({
-        contentIntent : Ti.Android.createPendingIntent({intent : intent}),
-        contentTitle : title,
-        contentText : message,
-    });
-    
-    // Send the notification
-    Ti.Android.NotificationManager.notify(2, notification);
-    
-    // Stop the service once the notification is sent
-    //Ti.Android.stopService(serviceIntent);
-} 
+
+// Grab extra data sent with the intent
+var title = serviceIntent.getStringExtra('title');
+var message = serviceIntent.getStringExtra('message');
+
+// Create an intent that launches the application
+var intent = Ti.Android.createIntent({
+	action : Ti.Android.ACTION_MAIN,
+	className : 'com.bd.localNotificationService.localNotificationServiceActivity',
+	packageName : 'com.bd.localNotificationService'
+});
+
+intent.flags |= Ti.Android.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED | Ti.Android.FLAG_ACTIVITY_SINGLE_TOP;
+intent.addCategory(Ti.Android.CATEGORY_LAUNCHER);
+
+function value() {
+	return Math.floor((Math.random() * 10) + 1).toString();
+}
+
+// Create notification
+var notification = Ti.Android.createNotification({
+	contentIntent : Ti.Android.createPendingIntent({
+		intent : intent
+	}),
+	contentTitle : value(),
+	contentText : message,
+});
+
+// Send the notification
+Ti.Android.NotificationManager.notify(1, notification);
+
+// Stop the service once the notification is sent
+//Ti.Android.stopService(serviceIntent);
